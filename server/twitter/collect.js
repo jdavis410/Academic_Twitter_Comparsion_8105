3ad57050c5a99d979d6f516a2b3d324653ae4717
Nicole Cantrell <nikkicantrell@lawn-128-61-0-221.lawn.gatech.edu>
2018-09-26 11:23:31 -0400

    Collects pages of tweets

--- server/twitter/collect.js
+++ server/twitter/collect.js
@@ -1,12 +1,30 @@
 var Twitter = require('twitter');
 var config = require('./config.js');
 var T = new Twitter(config);
-var params = { q: "#nodejs" }
-
-T.get('search/tweets', params, function(err, data, response) {
-  if(!err){
-    console.log(data);
-  } else {
-    console.log(err);
-  }
-})
+var hashtags = ["#1102kidsci"]; //collect these from database instead
+var lastid = 1; //get last id in database
+for(var i = 0; i < hashtags.length; i++) {
+    var maxId = 0;
+    var params = { q: hashtags[i], count: 100, since_id: lastid};
+    T.get('search/tweets', params, function(err, data, response) {
+        if(!err){
+            var tweets = data['statuses'];
+            console.log(tweets);
+            maxId = tweets[tweets.length - 1]['id'];
+        } else {
+            console.log(err);
+        }
+    });
+    while(maxId > lastid) {
+        var params = { q: hashtags[i], count: 100, since_id: lastid, max_id: maxId };
+        T.get('search/tweets', params, function(err, data, response) {
+            if(!err){
+                var tweets = data['statuses'];
+                console.log(tweets);
+                maxId = tweets[tweets.length - 1]['id'];
+            } else {
+                console.log(err);
+            }
+        });
+    }
+}
